b2961c3ae9606bd1e11b015fa6ad16763c06c77e
duenchay <duenchay.wo.60@ubu.ac.th>
2019-10-18 15:34:11 +0700

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,12 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { UsersComponent } from './users/users.component';
-import { LogComponent } from './log/log.component';
+// import { LogComponent } from './log/log.component';
+// import { PComponent } from './p/p.component';
+import { AngularFireModule} from 'angularfire2';
+import { firebaseConfig } from './../environments/firebase.config';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -21,12 +26,14 @@ import { LogComponent } from './log/log.component';
     HomeComponent,
     RegisterComponent,
     UsersComponent,
-    LogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
